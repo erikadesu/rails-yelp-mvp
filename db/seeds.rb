7713831bd1e3b42bbd60e4c47f96d046abf39844
Erika Kino <o.erika.o@gmail.com>
2018-07-25 16:05:10 -0300

    Create model Review

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: 1234567890,
+    category:     'belgian'
+
+
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: 1234567876,
+    category:     'italian'
+
+
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
