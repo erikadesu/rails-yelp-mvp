b37bce690106d1e7482abbcad56dd22847e34ccc
Erika Kino <o.erika.o@gmail.com>
2018-07-27 16:21:58 -0300

    Fix style

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,6 @@
 class Review < ApplicationRecord
+  validates :content, presence: true
+  validates :rating, inclusion: { in: 0..5, allow_nil: false, only_integer: true }
+  validates :restaurant_id, presence: true
   belongs_to :restaurant
 end
