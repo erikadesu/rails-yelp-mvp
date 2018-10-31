3ae6bc48e9786045afe6b8ed0665ab783b19c919
Erika Kino <o.erika.o@gmail.com>
2018-10-31 00:56:37 -0300

    fix new form

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,6 @@
 class Restaurant < ApplicationRecord
   validates :name, uniqueness: true, presence: true
   validates :address, presence: true
-  validates :category, inclusion: { in: %w(chinese, italian, french, belgian, japanese), allow_nil: false}, presence: true
+  validates :category, inclusion: { in: %w(chinese italian french belgian japanese), allow_nil: false}, presence: true
   has_many :reviews, dependent: :destroy
 end
